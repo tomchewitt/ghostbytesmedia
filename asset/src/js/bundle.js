087e04fb810559ccef2bc6d08263576ea20ad7b3
Sarah Hewitt <hello@sarahs.website>
2015-01-29 22:30:42 +0000

    Creating the index html.

--- asset/src/js/bundle.js
+++ asset/src/js/bundle.js
@@ -23,3 +23,55 @@
 // element.style.msTransform = "";
 // element.style.OTransform = "";
 // element.style.transform = "";
+
+// *****************************************************************
+// INIT
+// *****************************************************************
+
+
+// TEST FOR WEBKIT (blur??)
+// var isWebkit = 'WebkitAppearance' in document.documentElement.style
+// if (isWebkit) {
+// 	console.log('isWebkit');
+// } else {
+// 	console.log('isNotWebkit');
+// }
+
+
+// RESIZE
+// window.onresize = function() {
+	// document.querySelector('.loader').style.display = 'none';
+// };
+
+// var $ = function (selector) { return document.querySelector(selector) };
+// element.style.webkitTransform = "";
+// element.style.MozTransform = "";
+// element.style.msTransform = "";
+// element.style.OTransform = "";
+// element.style.transform = "";
+
+// *****************************************************************
+// INIT
+// *****************************************************************
+
+
+// TEST FOR WEBKIT (blur??)
+// var isWebkit = 'WebkitAppearance' in document.documentElement.style
+// if (isWebkit) {
+// 	console.log('isWebkit');
+// } else {
+// 	console.log('isNotWebkit');
+// }
+
+
+// RESIZE
+// window.onresize = function() {
+	// document.querySelector('.loader').style.display = 'none';
+// };
+
+// var $ = function (selector) { return document.querySelector(selector) };
+// element.style.webkitTransform = "";
+// element.style.MozTransform = "";
+// element.style.msTransform = "";
+// element.style.OTransform = "";
+// element.style.transform = "";
